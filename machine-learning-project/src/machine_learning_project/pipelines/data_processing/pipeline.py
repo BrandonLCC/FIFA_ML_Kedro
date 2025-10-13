888227b29e3a2cef45da4cc2a32b3d9f1e18215d
BrandonLCC <133028530+BrandonLCC@users.noreply.github.com>
2025-10-13 18:58:18 -0300

    Comentarios en pipeline.py

--- machine-learning-project/src/machine_learning_project/pipelines/data_processing/pipeline.py
+++ machine-learning-project/src/machine_learning_project/pipelines/data_processing/pipeline.py
@@ -18,24 +18,33 @@ from .nodes import preprocess_fifa_22, preprocess_fifa_21, preprocess_fifa_20
 def create_pipeline(**kwargs) -> Pipeline:
     return Pipeline(
         [
+            #Limpieza de los datos dataset 1
             Node(
                 func=preprocess_fifa_22, #funcion de node
                 inputs="DataSetFIFA22",
                 outputs="preprocess_fifa_22", # Definir las Salidas en catalog y aquí
                 name="preprocess_fifa_22_node",
             ),
+            #Limpieza de los datos dataset 2
             Node(
                 func=preprocess_fifa_21, 
                 inputs="DataSetFIFA21",
                 outputs="preprocess_fifa_21", 
                 name="preprocess_fifa_21_node",
             ),
+            #Limpieza de los datos dataset 3
             Node(
                 func=preprocess_fifa_20, 
                 inputs="DataSetFIFA20",
                 outputs="preprocess_fifa_20", 
                 name="preprocess_fifa_20_node",
             ),
+            
+            #Imputacion de los datos: valores missing (usar tecnicas como KNN, Valores Media mediana etc)
+
+            #Generacion de Feature 
+
+            #Node x : Union de las tablas
 
          
         ]

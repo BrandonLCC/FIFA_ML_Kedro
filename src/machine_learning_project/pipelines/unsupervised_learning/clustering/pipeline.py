1802c9c4e51272c3deb2bd39d650a4112636f5f3
BrandonLCC <br.casas@duocuc.cl>
2026-02-23 04:59:46 -0300

    Actualización de README.MD y agreagando archivos pdf

--- src/machine_learning_project/pipelines/unsupervised_learning/clustering/pipeline.py
+++ src/machine_learning_project/pipelines/unsupervised_learning/clustering/pipeline.py
@@ -8,6 +8,16 @@ from .nodes import (
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
+        # En clustering buscamos crear nuevas características basadas en la estructura de los datos, por lo que el input es el dataset limpio
+        #  y el output es un dataset con una nueva columna que representa el cluster asignado a cada muestra.
+        # Agregaremos estas características al dataset limpio para que 
+        # puedan ser utilizadas en el módulo de detección de anomalías.
+
+        # Clustering con KMeans, DBSCAN y Clustering Jerárquico
+        # El pipeline de reduccion de dimensionalizad sera el encargado de reducir la dimensionalidad 
+        # del dataset limpio, por lo que el input de los nodos de clustering sera el dataset limpio 
+        # con las nuevas características de reduccion de dimensionalidad.
+
         node(
             func=kmeans_clustering,
             inputs=["clean_dataset", "params:clustering.kmeans"],
@@ -37,4 +47,9 @@ def create_pipeline(**kwargs) -> Pipeline:
             outputs="clustered_unsupervised_processed_dataset",
             name="select_best_cluster_node"
         )
+
+        # Agregar al dataset limpio la nueva columna de cluster asignado para que pueda 
+        # ser utilizada en el módulo de detección de anomalías.
+
+      # usar deteccion antes o despues dek clustering?  
     ])
